c809bbdb62fd80c9df36665455779f1597e262d7
Pokkrong P <34023245+Nevioxis@users.noreply.github.com>
2023-09-14 15:12:52 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,4 +9,9 @@ app.get('/', (req, res) => {
     res.send({ "status": "ready" });
 });
 
+app.get('/time', (req, res) => {
+  const serverTime = new Date().toLocaleString();
+  res.send({ "time": serverTime });
+});
+
 app.listen(3000, () => console.log('listening'))
